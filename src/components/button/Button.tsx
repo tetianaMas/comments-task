41b945f12a861fbdb1c97c92bde557d4d49a4717
Tetiana Maslova <tetiana.maslova.qa@gmail.com>
2023-06-20 15:06:30 +0200

    feat: add form component and styles

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,8 +1,17 @@
-import React from "react"
-import { IButtonProps } from "./types"
+import React from 'react';
+import { IButtonProps } from './types';
+import './button.css';
 
-export const Button: React.FC<IButtonProps> = ({text, onClick}: IButtonProps) => {
-  return (<button onClick={onClick}>
-    {text}
-  </button>)
-}
+export const Button: React.FC<IButtonProps> = ({
+  text,
+  onClick,
+  type = 'button',
+  classNames = '',
+  isDisabled = false,
+}: IButtonProps) => {
+  return (
+    <button className={`button ${classNames}`} onClick={onClick} type={type} disabled={isDisabled}>
+      {text}
+    </button>
+  );
+};
--- src/components/button/types.ts
+++ src/components/button/types.ts
@@ -1,4 +1,7 @@
 export interface IButtonProps {
   text: string;
   onClick: () => void;
+  type?: 'button' | 'submit' | 'reset' | undefined;
+  classNames?: string;
+  isDisabled?: boolean;
 }
