6fb69a1a459ba63c07b88cc648c5841772e6072e
Tetiana Maslova <tetiana.maslova.qa@gmail.com>
2023-06-20 13:38:29 +0200

    feat: add fetching data from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { CommentModel } from './models';
+import { Comment } from './components/comment/Comment';
 
 export const App: React.FC = () => {
-  return <></>;
+  const [comments, setData] = useState<CommentModel[]>([]);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/comments')
+      .then((response) => response.json())
+      .then(({ comments }: { comments: CommentModel[] }) => setData(comments))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const commentNodes = comments?.map((commentItem: CommentModel) => (
+    <Comment key={commentItem.id} comment={commentItem} />
+  ));
+
+  return <div>{commentNodes}</div>;
 };
